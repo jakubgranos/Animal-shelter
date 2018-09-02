e96ee1a7a9784b6465cdf65027a10d6cecd02745
Jakub <jakubgranos@gmail.com>
2018-09-02 19:22:55 +0200

    poprawa wizualna sekcji carousel, poprawa jakosci zdjęc, dodanie pozostałych elementów h1 oraz przycisku do sekcji carousel

--- src/assets/js/modules/carousel.js
+++ src/assets/js/modules/carousel.js
@@ -1,13 +1,13 @@
 const carouselOptions = {
-    groupCells: 3,
-    pageDots: false,
-    arrowShape: { 
-        x0: 20,
-        x1: 50, y1: 25,
-        x2: 40, y2: 5,
-        x3: 85,
-        x4: 100, y4:50
-      }
+  groupCells: 3,
+  pageDots: false,
+  arrowShape: { 
+    x0: 20,
+    x1: 50, y1: 25,
+    x2: 40, y2: 5,
+    x3: 85,
+    x4: 100, y4:50
+  }
 };
     
 $('.carousel').flickity(carouselOptions);
